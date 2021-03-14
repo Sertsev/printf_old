a287edf660566aa7be041bee92d7bbc4a31a3703
candiepih <mutheeal.am@gmail.com>
2021-03-14 13:15:04 +0000

    initial commit

---
+++ holberton.h
@@ -0,0 +1,13 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+
+
+
+/**                                                                                                                                              
+ *printf function defination                                                                                                                     
+ */
+
+int _printf(const char *format, ...);
+
+#endif
