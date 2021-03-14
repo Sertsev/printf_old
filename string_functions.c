e3c7e9111bbff9d307341a2bc62e64c59c71e16b
candiepih <mutheeal.am@gmail.com>
2021-03-14 16:13:50 +0000

    added binary conversion file and string functions file

---
+++ string_functions.c
@@ -0,0 +1,39 @@
+/**
+ * _str_length - determines the length of the string
+ * @s: the string to get its length
+ * Return: string length
+ */
+
+int _str_length(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}
+
+/**
+ * rev_string - reverse string
+ * @s: string being reversed
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+	char temp;
+	int length, i;
+
+	i = 0;
+	length = (_str_length(s) - 1);
+
+	while (i < length)
+	{
+		temp = s[i];
+		s[i] = s[length];
+		s[length] = temp;
+		i++;
+		length--;
+	}
+}
---
+++ to_binary.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+/**
+ * intToBin - converts signed and unsinged ints to binary
+ * @digit: integer procided
+ * Return: conversion result as string
+ */
+
+char *intToBin(int digit)
+{
+	int b, k = 0;
+	char *bits;
+
+	bits = malloc(sizeof(char));
+	while (digit)
+	{
+		b = digit % 2;
+		digit = digit / 2;
+		bits[k] = b + '0';
+		k++;
+	}
+	rev_string(bits);
+
+	return (bits);
+}
