263fef823944aa8b82bc2fc25f947448fc9298fd
sertsev <sertseshewa@gmail.com>
2021-03-14 16:04:00 +0000

    updates

---
+++ holberton.h
@@ -0,0 +1,15 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ *printf function defination
+ */
+
+int _printf(const char *format, ...);
+
+#endif
