b4b2c0dfca21f705ec64df9b28b7df17a96dcf34
sertsev <sertseshewa@gmail.com>
2021-03-15 12:15:10 +0000

    trial

--- holberton.h
+++ holberton.h
@@ -1,15 +1,23 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+/**
+ * including necessary libraries
+ */
 
 #include <stdarg.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 /**
- *printf function defination
+ * a function defination
  */
 
 int _printf(const char *format, ...);
+char* _itoa(int value, char* result, int base);
+int _printf(const char *format, ...);
+int _mywrite(char* any, int count);
+void append(char* s, char c);
 
 #endif
